ace40cfa7a661d930b0f17a13fafb29a26bde286
mason z <s028214@students.lmsd.org>
2024-12-18 12:29:34 -0500

    Initial Commit

---
+++ Pawnhub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pawnhub
+//
+//  Created by Mason Zhu (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pawnhub/PawnhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawnhubApp.swift
+//  Pawnhub
+//
+//  Created by Mason Zhu (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PawnhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
